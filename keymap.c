6460c705b5d480e6f1985671e6ac23b45adadbdd
Clutchnp <clutch1356@proton.me>
2024-12-31 23:59:49 +0530

    some changes

--- keymap.c
+++ keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_1, KC_2, KC_3, KC_GRV,               KC_K, KC_O, KC_P, KC_W, KC_S,
         KC_A,    KC_4, KC_5, KC_6, KC_H,                 KC_H, KC_J, KC_K, KC_L, KC_F,
         KC_Z,    KC_SPC, KC_C, LSFT(KC_E), KC_B,         KC_I, KC_T, KC_M, LALT(KC_R), KC_D,
-                                KC_TRNS, KC_NO,           KC_LGUI, KC_O, KC_LSFT, KC_RALT
+                         KC_TRNS, KC_NO,KC_LGUI,            KC_O, KC_LSFT, KC_RALT
     ),
 
     // Layer 2: Symbols and Navigation
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_PAST, KC_1, KC_2, KC_3, KC_0,                 KC_WH_U, KC_F1, KC_F2, KC_F3, KC_F11,
         KC_PSLS, KC_4, KC_5, KC_6, KC_PPLS,              KC_F10, KC_F4, KC_F5, KC_F6, KC_END,
         KC_MINS, KC_7, KC_8, KC_9, KC_EQL,               KC_WH_D, KC_F7, KC_F8, KC_F9, KC_F12,
-                                KC_LGUI, KC_SPC,         KC_F13, KC_TRNS, KC_ENT, KC_TRNS
+                   KC_LCTL, KC_LALT,KC_LSFT,               KC_TRNS, KC_ENT, KC_TRNS
     ),
 
     // Layer 4: Alternative Symbols and Navigation
@@ -43,22 +43,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     // Layer 5: Custom Functions
-    [5] = LAYOUT_split_3x5_3(
-        LSFT(KC_Q), LCTL(LSFT(KC_W)), KC_NO, KC_NO, LSFT(KC_V),
-        LCTL(KC_TAB), KC_NO, KC_NO, KC_NO, LSFT(KC_A),
-        KC_NO, KC_NO, KC_F, KC_F13, LCTL(LSFT(KC_D)),
-                                KC_H, KC_J, KC_K, KC_L, KC_NO,
-                                LSFT(KC_Z), KC_NO, KC_NO, KC_NO, LCTL(LSFT(KC_T)),
-                                KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-                                KC_NO, KC_NO, KC_LGUI, KC_NO, KC_NO, KC_NO
-    ),
+[5] = LAYOUT_split_3x5_3(
 
-    // Layer 6: Navigation and Media
+    LSFT(KC_Q), LCTL(LSFT(KC_W)), LCTL(LSFT(KC_D)), KC_DEL, LSFT(KC_V),      LCTL(KC_TAB),KC_NO,KC_NO,KC_NO,LSFT(KC_A),
+    LCTL(LALT(KC_C)), KC_NO, KC_F, KC_F13, LCTL(LALT(KC_S)),             KC_H,KC_J,KC_K,KC_L,KC_NO,
+    LSFT(KC_Z),KC_NO, KC_NO, KC_NO, LCTL(LSFT(KC_T)),         KC_NO,KC_NO,KC_NO, KC_NO,KC_NO,
+                                 KC_NO,KC_NO,KC_LGUI,         KC_NO,KC_NO, KC_NO
+),
+
+
+    // Layer 6:
     [6] = LAYOUT_split_3x5_3(
         KC_TAB,  KC_Q, KC_W, KC_E, KC_R,                KC_NO, KC_NO, KC_UP, KC_NO, KC_TAB,
         KC_LSFT, KC_A, KC_S, KC_D, KC_F,                KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_RSFT,
         KC_LCTL, KC_Z, KC_X, KC_C, KC_V,                KC_NO, KC_NO, KC_NO, KC_NO, KC_RCTL,
-                                LM(8, MOD_LGUI), KC_SPC, MO(7), DF(0), KC_SPC, KC_PSCR
+                        LM(8, MOD_LGUI), KC_SPC,   MO(7), DF(0), KC_SPC, MO(9)
     ),
 
     // Layer 7: Numeric and Arrow Keys
@@ -75,6 +74,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_A, KC_4, KC_5, KC_6, KC_H,                 KC_H, KC_J, KC_K, KC_L, KC_F,
         KC_Z, KC_SPC, KC_C, LSFT(KC_E), KC_B,         KC_I, KC_T, KC_M, LALT(KC_R), KC_D,
                                 KC_TRNS, KC_NO,       KC_LGUI, KC_O, KC_LSFT, KC_RALT
+    ),
+    [9] = LAYOUT_split_3x5_3(
+        KC_PAST, KC_1, KC_2, KC_3, KC_0,                 KC_WH_U, KC_F1, KC_F2, KC_F3, KC_F11,
+        KC_PSLS, KC_4, KC_5, KC_6, KC_PPLS,              KC_F10, KC_F4, KC_F5, KC_F6, KC_END,
+        KC_MINS, KC_7, KC_8, KC_9, KC_EQL,               KC_WH_D, KC_F7, KC_F8, KC_F9, KC_F12,
+                   KC_LCTL, KC_LALT,KC_LSFT,               KC_TRNS, KC_ENT, KC_TRNS
     ),
 };
 

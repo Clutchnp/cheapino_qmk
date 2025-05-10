8e7b30ba5915438bbf368c0fe5e216c5d3156029
Clutchnp <clutch1356@proton.me>
2025-05-10 22:58:44 +0530

    arranging

--- keymap.c
+++ keymap.c
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LSFT_T(KC_Z), KC_X, KC_C, KC_V, KC_B,             KC_N, KC_M, KC_LALT, KC_DQUO, LCTL_T(KC_QUOT),
             LM(2, MOD_LGUI), KC_SPC,LT(6, KC_ESC),        MO(3), LT(3, KC_BSPC), MO(4)
     ),
-
+//Windows default layer
     [1] = LAYOUT_split_3x5_3(
         LCTL_T(KC_Q), KC_W, KC_E, KC_R, KC_T,             KC_Y, KC_U, KC_I, KC_O, KC_P,
         KC_A,         KC_S, KC_D, KC_F, KC_G,             KC_H, KC_J, KC_K, KC_L, KC_TAB,
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     LSFT(KC_Q), LCTL(LSFT(KC_W)), LCTL(LSFT(KC_D)), KC_DEL, LSFT(KC_V),      LCTL(KC_TAB),KC_NO,KC_NO,KC_NO,LSFT(KC_A),
     LSFT(KC_A),LCTL(LALT(KC_C)), KC_F, KC_F13, LCTL(LALT(KC_S)),             KC_H,KC_J,KC_K,KC_L,KC_NO,
-    LSFT(KC_Z),KC_NO, KC_NO, KC_NO, LCTL(LSFT(KC_T)),         KC_NO,KC_NO,KC_NO, KC_NO,KC_NO,
+    LSFT(KC_Z),KC_BTN1, KC_BTN2, KC_NO, LCTL(LSFT(KC_T)),         KC_NO,KC_NO,KC_NO, KC_NO,KC_NO,
                                  KC_NO,KC_NO,KC_LGUI,         KC_NO,KC_NO, KC_NO
 ),
 
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q, KC_W, KC_E, KC_R,                KC_NO, KC_NO, KC_UP, KC_NO, KC_TAB,
         KC_LSFT, KC_A, KC_S, KC_D, KC_F,                KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_RSFT,
         KC_LCTL, KC_Z, KC_X, KC_C, KC_V,                KC_NO, KC_NO, KC_NO, KC_NO, KC_RCTL,
-                        LM(9, MOD_LGUI), KC_SPC,   MO(8), DF(0), KC_SPC, MO(10)
+          LM(9, MOD_LGUI), KC_SPC,MO(8),                DF(0), KC_SPC, MO(10)
     ),
 
     // Layer 7: Numeric and Arrow Keys

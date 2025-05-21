466178b18c49c618875840d1cff78b80a3de7717
Clutchnp <clutch1356@proton.me>
2025-05-21 06:55:34 +0530

    changed sides of number layout extra fns for better coordination between symbols and numbers

--- keymap.c
+++ keymap.c
@@ -7,7 +7,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LCTL_T(KC_Q), KC_W, KC_E, KC_R, KC_T,             KC_Y, KC_U, KC_I, KC_O, KC_P,
         KC_A,         KC_S, KC_D, KC_F, KC_G,             KC_H, KC_J, KC_K, KC_L, KC_TAB,
         LSFT_T(KC_Z), KC_X, KC_C, KC_V, KC_B,             KC_N, KC_M, KC_LALT, KC_DQUO, LCTL_T(KC_QUOT),
-            LM(2, MOD_LGUI), KC_SPC,LT(6, KC_ESC),        MO(3), LT(3, KC_BSPC), MO(4)
+               LM(2, MOD_LGUI), KC_SPC,MO(4),             MO(3), LT(3, KC_BSPC), LT(6, KC_ESC)
     ),
 //Windows default layer
     [1] = LAYOUT_split_3x5_3(
@@ -28,17 +28,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // Layer 2: Symbols and Navigation
     [3] = LAYOUT_split_3x5_3(
-        KC_TILD, KC_EXLM, KC_AMPR, KC_HASH, KC_MINS,     KC_LBRC, KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN,
+        KC_TILD, KC_EXLM, KC_AMPR, KC_HASH, KC_EQL,     KC_LBRC, KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN,
         KC_GRV,  KC_DLR, KC_COMM, KC_DOT, KC_COLN,       KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_PERC,
-        KC_PLUS, KC_PIPE, KC_SLSH, KC_CIRC, KC_UNDS,     KC_RBRC, KC_EQL, KC_BSLS, KC_AT, KC_ASTR,
-                          KC_LSFT, KC_ENTER,KC_SCLN,     DF(1), KC_ENT, DF(7)
+        KC_SCLN, KC_PIPE, KC_SLSH, KC_CIRC, KC_UNDS,     KC_RBRC, KC_EQL, KC_BSLS, KC_AT, KC_ASTR,
+                          KC_LSFT, KC_ENTER,KC_PLUS,     DF(1), KC_ENT, DF(7)
     ),
 
     // Layer 3: Function Keys and Numpad
     [4] = LAYOUT_split_3x5_3(
         KC_PAST, KC_1, KC_2, KC_3, KC_0,                 KC_WH_U, KC_F1, KC_F2, KC_F3, KC_F11,
         KC_PSLS, KC_4, KC_5, KC_6, KC_PPLS,              KC_F10, KC_F4, KC_F5, KC_F6, KC_END,
-        KC_MINS, KC_7, KC_8, KC_9, KC_EQL,               KC_WH_D, KC_F7, KC_F8, KC_F9, KC_F12,
+        KC_MINS, KC_7, KC_8, KC_9, KC_MINS,               KC_WH_D, KC_F7, KC_F8, KC_F9, KC_F12,
                    KC_LCTL, KC_LALT,KC_LSFT,               KC_TRNS, KC_ENT, KC_TRNS
     ),
 

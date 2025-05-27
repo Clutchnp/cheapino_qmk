6d2033d85adfda2f2f62159010ffeb988e49349a
Clutchnp <clutch1356@proton.me>
2025-05-27 12:04:20 +0530

    home_row mods

--- keymap.c
+++ keymap.c
@@ -4,9 +4,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // Layer 0: Base Layer
     [0] = LAYOUT_split_3x5_3(
-        LCTL_T(KC_Q), KC_W, KC_E, KC_R, KC_T,             KC_Y, KC_U, KC_I, KC_O, KC_P,
-        KC_A,         KC_S, KC_D, KC_F, KC_G,             KC_H, KC_J, KC_K, KC_L, KC_TAB,
-        LSFT_T(KC_Z), KC_X, KC_C, KC_V, KC_B,             KC_N, KC_M, KC_LALT, KC_DQUO, LCTL_T(KC_QUOT),
+        KC_Q, KC_W, KC_E, KC_R, KC_T,                             KC_Y, KC_U, KC_I, KC_O, KC_P,
+        KC_A, LCTL_T(KC_S), LALT_T(KC_D), LSFT_T(KC_F), KC_G,     KC_H, LSFT_T(KC_J), LALT_T(KC_K), LCTL_T(KC_L), KC_TAB,
+        KC_Z, KC_X, KC_C, KC_V, KC_B,                             KC_N, KC_M, KC_LALT, KC_DQUO, LCTL_T(KC_QUOT),
                LM(2, MOD_LGUI), KC_SPC,MO(4),             MO(3), LT(3, KC_BSPC), LT(6, KC_ESC)
     ),
 //Windows default layer

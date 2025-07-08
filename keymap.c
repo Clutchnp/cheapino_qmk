f926ca27894cb218f8b6818a58e12713901a2781
Clutchnp <clutch1356@proton.me>
2025-07-08 06:39:52 +0530

    change again :( will probably do in 1week's time again also too:(

--- keymap.c
+++ keymap.c
@@ -30,9 +30,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Layer 0: Base Layer
     [0] = LAYOUT_split_3x5_3(
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,    KC_U,    KC_I,    KC_O,            KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,    KC_J,    KC_K,    KC_L,            KC_LSFT,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,    KC_M,    KC_DOUBLE_QUOTE, KC_QUOT, KC_TAB,
-        LM(3, MOD_LGUI),  KC_SPACE,         KC_LCTL,        KC_LALT, LT(1,KC_BSPC),    LT(4,KC_ESC)),
+        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,    KC_J,    KC_K,    KC_L,            KC_TAB,
+        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,    KC_M,    KC_DOUBLE_QUOTE, KC_QUOT, KC_LALT ,
+        LM(3, MOD_LGUI),  KC_SPACE,         KC_LCTL,        KC_LSFT, LT(1,KC_BSPC),    LT(4,KC_ESC)),
 
     // Layer 2: Symbols/Nav Layer
     [1] = LAYOUT_split_3x5_3(
